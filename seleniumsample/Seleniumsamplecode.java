e13b655cd64a9aa837f88ac2e40f20ba2443eb98
Arokia-Reshma <144796251+Arokia-Reshma@users.noreply.github.com>
2024-04-23 13:49:08 +0530

    Add files via upload

---
+++ seleniumsample/Seleniumsamplecode.java
@@ -0,0 +1,14 @@
+package seleniumsample;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Seleniumsamplecode {
+
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver(); 
+		driver.get("http://leaftaps.com/opentaps/control/main");
+
+	}
+
+}
+		
