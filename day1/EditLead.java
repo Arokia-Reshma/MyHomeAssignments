1bb991d0f020d066f89aa9f90d9b820522ec3fab
Arokia-Reshma <144796251+Arokia-Reshma@users.noreply.github.com>
2024-04-23 13:42:17 +0530

    Add files via upload

--- day1/EditLead.java
+++ day1/EditLead.java
@@ -1,37 +1,29 @@
-package week6.day1;
+package week5.day1;
 
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
 
-public class EditLead extends ProjectSpecificMethod {
-	
-	@BeforeTest
-	
-	public void setData() {
-		filename="Editlead";
-	}
-	
+public class EditLead extends ProjectSpecifiedMethod {
 	
 	@Test
 	
 
-	public  void runEditLead(String ph ,String upcom) throws InterruptedException {
+	public  void runEditLead() throws InterruptedException {
 		
 		driver.findElement(By.linkText("Leads")).click();
 		driver.findElement(By.linkText("Find Leads")).click();
 		driver.findElement(By.xpath("//span[text()='Phone']")).click();
-		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys(ph);
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("98");
 		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
 		driver.findElement(By.linkText("Edit")).click();
-		driver.findElement(By.id("updateLeadForm_companyName")).sendKeys(upcom);
+		driver.findElement(By.id("updateLeadForm_companyName")).sendKeys("TCS");
 		driver.findElement(By.name("submitButton")).click();
 		
 }
